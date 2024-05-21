8146465b704642053d2a982df83c1f4bff8ec9a4
Borgesfacundo <borgesfacundo@gmail.com>
2024-05-21 14:30:10 -0300

    asdasd

---
+++ Portfolio/scripts/scripts.js
@@ -0,0 +1,32 @@
+const skillsElement = document.querySelector(".skills");
+
+let skillList = {};
+
+const displaySkills = (skills) => {
+    skills.forEach(skill => {
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        const img = document.createElement("img");
+
+        p.textContent = `${skill.name}`;
+        img.setAttribute("src", skill.img);
+        img.setAttribute("alt", skill.desc);
+
+        div.appendChild(p);
+        div.appendChild(img)
+
+        skillsElement.appendChild(div);
+    })
+}
+
+const getSkills = async () => {
+    const response = await fetch("https://borgesfacundo.github.io/Projects/Portfolio/json/skills.json");
+    if (response.ok) {
+        const data = await response.json();
+        skillList = data;
+
+        displaySkills(skillList);
+    }
+}
+
+getSkills();
